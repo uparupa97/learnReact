7eb6a7f3ce60f4e82cbb13377951904963526b1c
turtle <uparupa97@icloud.com>
2024-09-06 17:25:30 +0900

    240906 react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
-function firstAction() {
+function Firstaction() {
+// logic 
+console.log("App.js")
+
+const name = "우파루파"; 
+const age = 28; 
+
+const HI = `안녕하세요. ${name}님! 나이가 ${age}이시네요. 
+환영합니다`;
+
+console.log(HI);
+
+const user = {
+  isLoggedin: true, 
+  role: "admin" //'guest', 'user'
+}
+
+//사용자가 로그인한 상태이면서, 역할이 admin인 경우에는 관리자페이지 접근 가능
+
+if (user.isLoggedin && user.role === 'admin') {
+  console.log("관리자 페이지 접근 가능")
+} else {console.log("관리자 페이지 접근 불가 ");}
+
+//view 
+
   return <div>Hello world!</div>;
 }
 
-export default firstAction;
+export default Firstaction;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import firstAction from './App';
+import Firstaction from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <firstAction/>
+    <Firstaction/>
   </React.StrictMode>
 );
 
