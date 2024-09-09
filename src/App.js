d588085ab7dca3d0fc56bc713540141b1a424c83
turtle <uparupa97@icloud.com>
2024-09-09 14:02:28 +0900

    jsx file first made

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+
+import Carrd from './coMPonents/Carrd';
+
 function Firstaction() {
 // logic 
 console.log("App.js")
@@ -15,6 +18,92 @@ const user = {
   role: "admin" //'guest', 'user'
 }
 
+const greeting = (id = '고객') => { 
+  return `${id}님 안녕하신지요`;
+}
+
+console.log('안녕', greeting('영현'));
+
+const learnlist = ['HTML', 'CSS', 'JAVA', 'REACT'];
+
+for (const element of learnlist) { 
+  console.log(element, '을 공부하고 있다.');
+}
+
+const addLan = () => { 
+  learnlist.push('Github');
+  const newlist = ['python', ...learnlist]; 
+  console.log(learnlist); 
+  console.log(newlist); 
+}
+
+addLan();
+
+
+const [firstlan, secondlan, thirdlan, fourthlan] = learnlist; 
+
+console.log(firstlan, secondlan, thirdlan, fourthlan); 
+
+
+const id_number = {
+  id : 'youngshin',
+  password: 1234,
+  email: 'fgghg2013@naver.com'
+}; 
+
+
+const id_number2 = {
+  id : 'uparupa', 
+  password: 3456, 
+  email: 'uparupa97@icloud.com'
+};
+
+const id_message = { 
+  message: '안녕하세요'
+};
+
+
+const newName = () => { 
+  id_number.id = 'uparupa97'; 
+  console.log(id_number);
+}
+
+newName();
+
+const addMessage = {
+  ...id_number, 
+  ...id_message, 
+  name : "주영신", 
+  id: '바꿔볼까'
+}; 
+
+console.log(addMessage); 
+
+const addID = () => { 
+  const idList = []; 
+  idList.push(addMessage); 
+  idList.push(id_number2); 
+  console.log(idList); 
+}
+
+addID(); 
+
+
+
+// const {id, password, email} = id_number; 
+
+// console.log(id, password, email); 
+
+// const addId = () => { 
+//   const newid = {...id_message, ...id_number, name: "fgghg2013"};  
+//   console.log(newid); 
+// }
+
+// addId();
+
+
+
+
 //사용자가 로그인한 상태이면서, 역할이 admin인 경우에는 관리자페이지 접근 가능
 
 if (user.isLoggedin && user.role === 'admin') {
@@ -23,7 +112,12 @@ if (user.isLoggedin && user.role === 'admin') {
 
 //view 
 
-  return <div>Hello world!</div>;
+  return (
+    <>
+    <Carrd/>
+  </>
+  ); 
+
 }
 
 export default Firstaction;
---
+++ src/coMPonents/Carrd.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Carrd = () => {
+  return (
+    <div id="CardComponent">Carrd</div>
+  )
+}
+
+export default Carrd
