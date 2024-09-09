ffdafbc4b833af8732aab36fd883e9d430c560d0
turtle <uparupa97@icloud.com>
2024-09-09 16:47:54 +0900

    google logo

--- src/coMPonents/SocialButton.jsx
+++ src/coMPonents/SocialButton.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import GoogleLogo from '/Users/mac/Desktop/ project-visualstudiocode/LDuparupa97/reactfirstday/src/image/GoogleLogo.png';
+
 
 const SocialButton = () => {
   return (
     <div >
     <button type="button" className="SocialButton">
+        <img className="Google" alt="G" src={GoogleLogo} />
         Continue with google
     </button>
  </div>
