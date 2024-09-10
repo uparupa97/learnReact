18458a66d83212b0bb6eec87b5888b7cf2aff334
turtle <uparupa97@icloud.com>
2024-09-10 16:23:53 +0900

    prop and state

--- src/coMPonents/CardButton.jsx
+++ src/coMPonents/CardButton.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 
-const CardButton = () => {
+const CardButton = ({handleClick}) => {
 
+  
 
-
+  const firstClick =  () => {
+    console.log("cardbutton -> click!!"); 
+    handleClick(); 
+  }
 
 //view
   return (
     <div >
-       <button type="button" className="cardButton">Go somewhere</button>
+        {/* <button type="button" className="cardButton" onClick={handleButton}>Go somewhere</button> */}
+
+       <button type="button" className="cardButton" onClick={firstClick}>Go somewhere</button>
     </div>
   )
 }
--- src/coMPonents/Carrd.jsx
+++ src/coMPonents/Carrd.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import CardButton from './CardButton'
 
-const Carrd = ({imageLink, title, subText}) => {
+const Carrd = ({imageLink, title, subText, onCardbuttonClick}) => {
     //logic 
     // const subText = "This is subtext";
     // const name = "김우빈";
 
+    const yoohoClick = () => {
+      console.log("Carrd Click!"); 
+      onCardbuttonClick(); 
+    }
+
+    
     //view
   return (
     <div className="card">
         <img className="kimwoobin" src={imageLink} alt="김우빈"/>
         <h5 className="cardTitle">{title}</h5>
         <p className="cardSubText"> {subText}</p>
-        <CardButton/>
+        <CardButton handleClick={yoohoClick} />
+        {/* <CardButton onClick={handleClick}> */}
     </div>
   )
 }
